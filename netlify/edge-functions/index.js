1a260837da739eb41c32161b0147faac8f9e11e1
David Adamson <david.adamson2304@gmail.com>
2024-08-07 19:24:41 +0200

    Revert "fix"
    This reverts commit 3adb0cad3e008d44339cfadc8aa61cf1c11f501f.

--- netlify/edge-functions/index.js
+++
@@ -1,6 +0,0 @@
-import { initApp } from "../../src/server/main";
-import { toWebHandler } from "h3";
-
-export const config = { path: "/api/*" };
-// eslint-disable-next-line
-export default toWebHandler(initApp(Netlify.env.toObject()).h3App);
