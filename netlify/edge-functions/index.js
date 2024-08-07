f8b8f64c596d913d03ce73e2fb5e66e84e1bd851
David Adamson <david.adamson2304@gmail.com>
2024-08-07 18:00:28 +0200

    fix

--- netlify/edge-functions/index.js
+++ netlify/edge-functions/index.js
@@ -2,4 +2,4 @@ import app from "../../.output/app.mjs";
 import { toWebHandler } from "h3";
 
 export const config = { path: "/api/*" };
-export const handler = toWebHandler(app.h3App);
+export default toWebHandler(app.h3App);
